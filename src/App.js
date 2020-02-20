93911fac6cbff114269f11f78efe28fdde64416e
Pedro <pedrobusou@gmail.com>
2020-02-21 00:00:13 +0100

    Setup columnts and create base Form component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import Form from './components/Form';
 
-function App() {
-  return <h1>Patient manager</h1>;
-}
+const App = () => (
+  <Fragment>
+    <h1>Patient manager</h1>
+
+    <div className="container">
+      <div className="row">
+        <div className="one-half column">
+          <Form />
+        </div>
+
+        <div className="one-half column">2</div>
+      </div>
+    </div>
+  </Fragment>
+);
 
 export default App;
