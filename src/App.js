aed5ae8576e4551d5422d5eba3eb5b7132e97c86
Pedro <pedrobusou@gmail.com>
2020-02-21 21:22:09 +0100

    Create folder for Appointment Extract appointment scss into its own file.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Fragment, useState} from 'react';
 import Form from './components/Form';
-import Appointment from './components/Appointment';
+import Appointment from './components/appointment/Appointment';
 
 const App = () => {
   const [appointments, setAppointments] = useState([]);
--- src/components/appointment/Appointment.js
+++ src/components/appointment/Appointment.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './appointment.scss';
 
 const Appointment = ({
   appointment: {petName},
